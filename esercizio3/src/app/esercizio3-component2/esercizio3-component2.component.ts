9adf2f2fbb0e69baeeaa76a2db1447a86d1f30b5
Alexander Santiana <santiana.a@iisgalvanimi.edu.it>
2024-01-31 08:25:36 +0000

    2.0

--- esercizio3/src/app/esercizio3-component2/esercizio3-component2.component.ts
+++ esercizio3/src/app/esercizio3-component2/esercizio3-component2.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-esercizio3-component2',
   standalone: true,
-  imports: [],
+  imports: [MatProgressBarModule],
   templateUrl: './esercizio3-component2.component.html',
   styleUrl: './esercizio3-component2.component.css'
 })
 export class Esercizio3Component2Component {
+  nlike: number = 0;
 
+  function nrandom(min:number, max:number) {
+    return Math.random() * (max - min)
+  }
+
+  constructor() {
+    console.log('Il componente è stato inizializzato')
+  }
 }
