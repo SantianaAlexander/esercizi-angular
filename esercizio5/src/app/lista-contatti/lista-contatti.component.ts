9e7e69c4a423ee4a9971b395c6746a258cd37dfb
Alexander Santiana <santiana.a@iisgalvanimi.edu.it>
2024-03-05 19:37:22 +0000

    1.0.1

--- esercizio5/src/app/lista-contatti/lista-contatti.component.ts
+++ esercizio5/src/app/lista-contatti/lista-contatti.component.ts
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './lista-contatti.component.css'
 })
 export class ListaContattiComponent {
-  
+  persone = [
+    {nome: "Jonathan", cognome: "Borgheresi", numero: 17759865200, propic: "https://t4.ftcdn.net/jpg/04/44/53/99/360_F_444539901_2GSnvmTX14LELJ6edPudUsarbcytOEgj.jpg"},
+    {nome: "John", cognome: "Magic", numero: 32468799974, propic: "https://t3.ftcdn.net/jpg/03/02/09/52/360_F_302095207_iuCeDS2jIkIptfE29Wb0ldDs248FAzoi.jpg"},
+    {nome: "Maria", cognome: "Perak", numero: 5511953259340, propic: "https://images.pexels.com/photos/3763188/pexels-photo-3763188.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"},
+    {nome: "Marco", cognome: "Mavco", numero: 528124129087, propic: "https://media.istockphoto.com/id/1300512215/photo/headshot-portrait-of-smiling-ethnic-businessman-in-office.jpg?s=612x612&w=0&k=20&c=QjebAlXBgee05B3rcLDAtOaMtmdLjtZ5Yg9IJoiy-VY="},
+    {nome: "Boban", cognome: "Penzolov", numero: 37129159431, propic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThe_GdeYkM6l3hHHFoXVlZKF2uqOC9LU049A&usqp=CAU"}
+    
+  ]
 }
