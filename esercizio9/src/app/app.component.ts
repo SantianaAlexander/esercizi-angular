fe06f8611bf94d0b3f2094b3b0f46a2d7900e996
Alexander Santiana <santiana.a@iisgalvanimi.edu.it>
2024-04-11 21:13:55 +0000

    Vers. 1.0.3

--- esercizio9/src/app/app.component.ts
+++ esercizio9/src/app/app.component.ts
@@ -2,15 +2,27 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
 import { IntroductionComponent } from './introduction/introduction.component';
+import { CommonModule } from '@angular/common';
+import { ListComponent } from './list/list.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationComponent, IntroductionComponent],
+  imports: [RouterOutlet, NavigationComponent, IntroductionComponent, CommonModule, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'esercizio9';
+  pagina: string = "welcome"
+
+  onRiceviDati(value: any){
+    console.log(value)
+    this.pagina = value
+  }
+  onRiceviDati2(value: any) {
+    console.log(value)
+    this.pagina = value
+  }
 }
--- esercizio9/src/app/navigation/navigation.component.ts
+++ esercizio9/src/app/navigation/navigation.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
 export class NavigationComponent {
-
+  @Output() mandaDatiEvento = new EventEmitter<string>();
+  @Output() mandaDatiEvento2 = new EventEmitter<string>();
+  pagina = "welcome"
+  apriHome() {
+    this.pagina = "welcome"
+    this.mandaDatiEvento.emit(this.pagina);
+  }
+  apriBiglietti() {
+    this.pagina = "biglietti"
+    this.mandaDatiEvento2.emit(this.pagina);
+  }
 }
