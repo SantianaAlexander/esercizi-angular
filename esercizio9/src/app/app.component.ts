65580599c2b44955c020d05e76554bd9d92e3a04
Alexander Santiana <santiana.a@iisgalvanimi.edu.it>
2024-04-11 12:41:03 +0000

    Vers. 1.0.1

--- esercizio9/src/app/app.component.ts
+++ esercizio9/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
+import { IntroductionComponent } from './introduction/introduction.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationComponent],
+  imports: [RouterOutlet, NavigationComponent, IntroductionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
