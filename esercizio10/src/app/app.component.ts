06726bb7a4b8d01b23db2ff72c20bc4c0da46c46
Alexander Santiana <santiana.a@iisgalvanimi.edu.it>
2024-04-21 19:51:05 +0000

    Vers. 1.0.0

---
+++ esercizio10/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {MatDividerModule} from '@angular/material/divider'
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MatDividerModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'esercizio10';
+}
