8f954c11be5145a1b07fb726efc78e3b2cffb047
Alexander Santiana <santiana.a@iisgalvanimi.edu.it>
2024-04-22 18:17:08 +0000

    Vers. 1.0.1

--- esercizio10/src/app/app.component.ts
+++ esercizio10/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import {MatDividerModule} from '@angular/material/divider'
+import { Routes } from '@angular/router';
+import { MostreComponent } from './mostre/mostre.component';
+import { HomeComponent } from './home/home.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatDividerModule],
+  imports: [RouterOutlet, MatDividerModule, HomeComponent, RouterLink, MostreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- esercizio10/src/app/app.routes.ts
+++ esercizio10/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MostreComponent } from './mostre/mostre.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'Mostre', component: MostreComponent},
+    {path: 'Home', component: HomeComponent},
+    {path: '', redirectTo: '/Home', pathMatch: 'full'}
+];
